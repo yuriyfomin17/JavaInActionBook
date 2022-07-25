1b1504d25da7d1e87d5e09804a0014da2878feed
yuriyfomin17 <fominyura2012@gmail.com>
2022-07-25 11:27:59 +0300

    commit

--- src/chapter2/Apple.java
+++ src/chapter2/Apple.java
@@ -2,12 +2,20 @@ package chapter2;
 
 import chapter2.util.*;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.function.BiFunction;
 import java.util.function.Predicate;
 
 public class Apple implements ApplePredicate {
     private int weight = 0;
     private Color color = Color.getRandomColor();
+
+    public Apple(){}
+    public Apple(Integer weight, Color color){
+        this.weight = weight;
+        this.color = color;
+    }
 
     public int getWeight() {
         return weight;
@@ -57,5 +65,13 @@ public class Apple implements ApplePredicate {
 
     public static List<Apple> filterApples(List<Apple> appleList, Predicate<Apple> applePredicate){
         return appleList.stream().filter(applePredicate).toList();
+    }
+    public static List<Apple> createApples(List<Integer> weights, List<Color> colors, BiFunction<Integer, Color, Apple> appleBiFunction){
+        int minSize = Math.min(weights.size(), colors.size());
+        List<Apple> result = new ArrayList<>();
+        for (int i = 0; i < minSize; i++) {
+            result.add(appleBiFunction.apply(weights.get(i), colors.get(i) ));
+        }
+        return result;
     }
 }
--- src/chapter2/test/AppleTest.java
+++ src/chapter2/test/AppleTest.java
@@ -1,10 +1,12 @@
 package chapter2.test;
 import chapter2.Apple;
 import chapter2.util.AppleDataGenerator;
+import chapter2.util.Color;
 import org.junit.jupiter.api.Test;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class AppleTest {
 
@@ -42,6 +44,52 @@ public class AppleTest {
         Comparator<Apple> appleComparator = Comparator.comparingInt(Apple::getWeight);
         apples.sort(appleComparator);
         apples.forEach(System.out::println);
+    }
+    @Test
+    public void sortApplesByMethodReferences(){
+        List<Apple> apples = AppleDataGenerator.getListOfRandomApples(20);
+        apples.sort(Comparator.comparing(Apple::getWeight));
+        apples.forEach(System.out::println);
+    }
+
+    @Test
+    public void sortApplesByWeightAndColor(){
+        List<Apple> apples = AppleDataGenerator.getListOfRandomApples(20);
+        apples.sort(Comparator.comparing(Apple::getWeight).thenComparing(Apple::getColor).reversed());
+        apples.forEach(System.out::println);
+    }
+
+    @Test
+    public void createNewApples(){
+        List<Integer> weights = List.of(100, 110, 120, 130, 140, 150);
+        List<Color> colors = List.of(Color.RED, Color.BLUE, Color.ORANGE, Color.GREEN, Color.INDIGO, Color.PINK);
+
+        List<Apple> apples = Apple.createApples(weights, colors, Apple::new);
+        apples.forEach(System.out::println);
+    }
+
+    @Test
+    public void negatePredicate(){
+        Predicate<Apple> redApple = Apple::isAppleRed;
+        Predicate<Apple> notRedApple = redApple.negate();
+        Predicate<Apple> redAndHeavyApple = redApple.and(Apple::isAppleHeavy);
+        Predicate<Apple> greenOrRedAndHeavyApple = redAndHeavyApple.or(Apple::isAppleGreen);
+        List<Apple> apples = AppleDataGenerator.getListOfRandomApples(30);
+
+        List<Apple> redAndHeavyApples = Apple.filterApples(apples, redAndHeavyApple);
+        System.out.println("RED AND HEAVY APPLES");
+        redAndHeavyApples.forEach(System.out::println);
+
+        List<Apple> notReadApples = Apple.filterApples(apples, notRedApple);
+        System.out.println("NOT RED APPLES");
+        notReadApples.forEach(System.out::println);
+
+
+        List<Apple> greenOrRedAndHeavyApples = Apple.filterApples(apples, greenOrRedAndHeavyApple);
+        System.out.println("GREEN OR RED AND HEAVY APPLES");
+        greenOrRedAndHeavyApples.forEach(System.out::println);
+
+
     }
 
 }
